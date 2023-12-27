ca528a8c4bed10177f6143f2aecaed7f1f1985d5
vijaykumar1360 <152047332+vijaykumar1360@users.noreply.github.com>
2023-12-27 11:05:50 +0530

    Update Arduino.h

--- Arduino.h
+++ Arduino.h
@@ -1,104 +1,80 @@
 #include <Arduino.h>
 
-// Motor control pins
-const int pwmPin = 5;  // PWM pin connected to H-Bridge
-const int in1Pin = 2;  // H-Bridge input 1
-const int in2Pin = 4;  // H-Bridge input 2
+// Constants
+const int motorPWM = 5;    // PWM pin connected to H-Bridge
+const int encoderPinA = 2; // Hall effect encoder channel A
+const int encoderPinB = 3; // Hall effect encoder channel B
 
-// Encoder pins
-const int encoderPinA = 14;  // Hall-effect encoder channel A
-const int encoderPinB = 12;  // Hall-effect encoder channel B
+const int encoderResolution = 4096; // Updated pulses per revolution
+const float pulsesPerDegree = static_cast<float>(encoderResolution) / 360.0;
 
-// Serial communication
-const int serialSpeed = 9600;
+volatile int encoderCount = 0;  // Counter for encoder pulses
+int targetSpeed = 0;
 
-// Motor parameters
-const int maxSpeed = 255;
-const int minSpeed = 0;
+// Motor Specifications
+const int noLoadSpeed = 300;   // RPM
 
-// Variables
-int motorSpeed = 0;
-volatile long encoderCount = 0;  // Counter for encoder pulses
+// Function prototypes
+void setMotorSpeed(int pwmDutyCycle);
+void processSerialCommand();
+int calculatePWMDutyCycle(int targetSpeed);
+float calculatePulsesPerDegree();
 
 void setup() {
-  // Motor setup
-  pinMode(pwmPin, OUTPUT);
-  pinMode(in1Pin, OUTPUT);
-  pinMode(in2Pin, OUTPUT);
+  pinMode(motorPWM, OUTPUT);
+  pinMode(encoderPinA, INPUT_PULLUP);
+  pinMode(encoderPinB, INPUT_PULLUP);
 
-  // Encoder setup
-  pinMode(encoderPinA, INPUT);
-  pinMode(encoderPinB, INPUT);
-  attachInterrupt(digitalPinToInterrupt(encoderPinA), updateEncoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoderPinA), updateEncoder, RISING);
 
-  // Serial setup
-  Serial.begin(serialSpeed);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // Read user input
-  if (Serial.available() > 0) {
-    char command = Serial.read();
-    int speedValue = 0;
-
-    // Parse user command
-    switch (command) {
-      case 'F':
-        speedValue = readSpeedFromSerial();
-        setMotorSpeed(speedValue);
-        break;
-      case 'B':
-        speedValue = readSpeedFromSerial();
-        setMotorSpeed(-speedValue);  // Negative speed for backward
-        break;
-      case 'S':
-        stopMotor();
-        break;
-    }
-  }
-
-  // Implement closed-loop motor control using encoder feedback
-  // Adjust the PID control parameters as needed
-  // For simplicity, proportional control is used here
-  int targetEncoderCount = map(motorSpeed, -maxSpeed, maxSpeed, -500, 500);
-  int error = targetEncoderCount - encoderCount;
-  int pidOutput = constrain(error, -255, 255);
-
-  analogWrite(pwmPin, abs(pidOutput));
-
-  if (pidOutput >= 0) {
-    digitalWrite(in1Pin, HIGH);
-    digitalWrite(in2Pin, LOW);
-  } else {
-    digitalWrite(in1Pin, LOW);
-    digitalWrite(in2Pin, HIGH);
-  }
+  processSerialCommand();
+  int pwmDutyCycle = calculatePWMDutyCycle(targetSpeed);
+  setMotorSpeed(pwmDutyCycle);
 }
 
-int readSpeedFromSerial() {
-  int speedValue = 0;
-  while (Serial.available() > 0) {
-    char digit = Serial.read();
-    if (digit >= '0' && digit <= '9') {
-      speedValue = speedValue * 10 + (digit - '0');
-    }
-  }
-  return constrain(speedValue, minSpeed, maxSpeed);
-}
-
-void setMotorSpeed(int speed) {
-  motorSpeed = constrain(speed, -maxSpeed, maxSpeed);
-}
-
-void stopMotor() {
-  setMotorSpeed(0);
-}
-
-// Interrupt service routine for encoder counting
 void updateEncoder() {
   if (digitalRead(encoderPinB) == HIGH) {
     encoderCount++;
   } else {
     encoderCount--;
   }
+}
+
+void setMotorSpeed(int pwmDutyCycle) {
+  analogWrite(motorPWM, pwmDutyCycle);
+}
+
+void processSerialCommand() {
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    switch (command) {
+      case 'F':
+        targetSpeed = Serial.parseInt();
+        break;
+      case 'B':
+        targetSpeed = -Serial.parseInt();
+        break;
+      case 'S':
+        targetSpeed = 0;
+        break;
+      default:
+        // Handle invalid command
+        break;
+    }
+  }
+}
+
+int calculatePWMDutyCycle(int targetSpeed) {
+  // Map target speed to PWM duty cycle using motor specifications
+  float speedRatio = static_cast<float>(targetSpeed) / noLoadSpeed;
+  int pwmDutyCycle = constrain(static_cast<int>(speedRatio * 255), 0, 255);
+  return pwmDutyCycle;
+}
+
+float calculatePulsesPerDegree() {
+  return pulsesPerDegree;
 }
